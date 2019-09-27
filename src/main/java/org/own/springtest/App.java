16299d62513c2af00ab50ba9bd6edb9fc54f2ac1
Jose Osorio Soto <jose_osorio_soto@claro.com.do>
2019-09-27 14:07:21 -0400

    Core Container and respectively dependencys already set. XML File Fully Configured

--- src/main/java/org/own/springtest/App.java
+++ src/main/java/org/own/springtest/App.java
@@ -13,7 +13,7 @@ public class App
 {
     public static void main( String[] args ) throws Exception
     {
-        BeanFactory factory = new XmlBeanFactory(new FileInputStream("name"));
-    
+
+        final BeanFactory factory = new XmlBeanFactory(new FileInputStream("src\\main\\java\\org\\own\\springtest\\hello.xml"));
     }
 }
